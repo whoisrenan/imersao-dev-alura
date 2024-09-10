a40e5061580867f6dfdceaebd85643f64fbb7fba
Renan Lemos <renanlemos1612@gmail.com>
2024-09-10 13:58:44 -0300

    projeto-finalizado

--- dados.js
+++ dados.js
@@ -2,19 +2,22 @@ let dados  = [
   {
   titulo: "Rebeca Andrade",
   descricao: "Rebeca Rodrigues de Andrade é uma ginasta artística brasileira que se tornou um dos maiores nomes do esporte no país. Nascida em Guarulhos, São Paulo, em 1999, Rebeca conquistou o coração dos brasileiros com sua determinação, talento e superação.",
-  link: "https://google.com/search?q=rebeca+andrade"
+  link: "https://google.com/search?q=rebeca+andrade",
+  tags: "ginástica ginastica ginasta ouro",
 },
 
 {
   titulo: "Rayssa Leal",
   descricao: "A mais nova medalhista olímpica brasileira, Rayssa Leal, conquistou o mundo com seu skate. Nascida em 2008, em Florianópolis, ela é um fenômeno do skate e um exemplo de superação para jovens atletas.",
-  link: "https://google.com/search?q=rayssa+leal"
+  link: "https://google.com/search?q=rayssa+leal",
+  tags: "skate fada sk8",
 },
 
 {
   titulo: "Bia Souza",
   descricao: "A judoca Bia Souza fez história ao conquistar a primeira medalha de ouro para o Brasil nos Jogos Olímpicos de Paris 2024. Com sua técnica impecável e determinação, ela se tornou um dos principais nomes do judô brasileiro.",
-  link: "https://google.com/search?q=beatriz+souza"
+  link: "https://google.com/search?q=beatriz+souza",
+  tags: "judô judo judoka",
 }, 
 ]
 
--- script.js
+++ script.js
@@ -1,18 +1,45 @@
 function pesquisar() {
+  // Seleciona a seção HTML onde os resultados serão exibidos
   let section = document.getElementById("resultados-pesquisa");
-  console.log(section);
 
-  let resultados = "";
-  // para cada dado dentro da lista de dados
-  for (let dado of dados) {
-    resultados += `
-  <div class="item-resultado">
-    <h2>${dado.titulo}</h2>
-    <p class="descricao-meta">
-    ${dado.descricao}
-    </p>
-    <a href="${dado.link}" target="_blank"> Saiba mais</a>
-  </div>`;
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+  campoPesquisa = campoPesquisa.toLowerCase();
+
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>Nenhum atleta encontrado</p>"
+    return;
   }
+
+  // Inicializa uma string vazia para armazenar os resultados HTML
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+
+  // Itera sobre cada item de dado (assumindo que 'dados' é um array de objetos)
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase()
+    // se o titulo includes campoPesquisa
+    // então será executado o código entre chaves.
+
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      // Constrói o HTML para cada item, formatando com template literals
+      resultados += `
+      <div class="item-resultado">
+        <h2>${dado.titulo}</h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a href="${dado.link}" target="_blank">Saiba mais</a>
+      </div>
+    `;
+    }
+  }
+
+  if (!resultados) {
+    resultados = "<p> Nada foi encontrado! Você precisa digitar o nome de um atleta ou esporte.</p>"
+  }
+  // Atribui o HTML gerado para a seção, substituindo o conteúdo anterior
   section.innerHTML = resultados;
 }
